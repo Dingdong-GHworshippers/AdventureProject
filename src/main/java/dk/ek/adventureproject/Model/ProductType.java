b869974fb55de9ab3ae66d6216fe59ff3381a064
Thomas-dahl-01 <thda0002@stud.ek.dk>
2025-09-30 11:37:41 +0200

    Fixed Productype enums

---
+++ src/main/java/dk/ek/adventureproject/Model/ProductType.java
@@ -0,0 +1,19 @@
+package dk.ek.adventureproject.Model;
+
+public enum ProductType {
+    FOOD ("Mad"),
+    DRINK ("Drikkevarer"),
+    MERCHANDISE ("Merch");
+
+    private final String prettyprint;
+
+    ProductType(String prettyprint) {
+        this.prettyprint = prettyprint;
+    }
+
+    @Override
+    public String toString() {
+        return prettyprint;
+    }
+
+}
