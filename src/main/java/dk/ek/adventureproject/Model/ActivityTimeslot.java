4557315b0cec28c76156ef03117b45d48fb74456
Noahthomas24 <noth0001@stud.kea.dk>
2025-10-01 22:58:30 +0200

    Added relation between ActivityTimeslot and employee so that employees should now be able to get added to an activitytimeslot. Also added potential for future employee rosterplan.

--- src/main/java/dk/ek/adventureproject/Model/ActivityTimeslot.java
+++ src/main/java/dk/ek/adventureproject/Model/ActivityTimeslot.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,16 +14,13 @@ import java.time.LocalDateTime;
 @Table (name = "activity_timeslots")
 public class ActivityTimeslot {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDateTime startTime;
     private LocalDateTime endTime;
   
-    public ActivityTimeslot(Long id, LocalDateTime startTime, LocalDateTime endTime) {
-      this.id = id;
-      this.startTime = startTime;
-      this.endTime = endTime;
-    }
+
 
     @ManyToOne
     @JoinColumn (name = "activity_id")
@@ -30,6 +29,22 @@ public class ActivityTimeslot {
     @ManyToOne
     @JoinColumn (name = "booking_id")
     private Booking booking;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "activity_timeslot_employees",
+            joinColumns = @JoinColumn(name = "timeslot_id"),
+            inverseJoinColumns = @JoinColumn(name = "employee_id")
+    )
+    private List<Employee> employees = new ArrayList<>();
+
+
+    public ActivityTimeslot(Long id, LocalDateTime startTime, LocalDateTime endTime) {
+        this.id = id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
     public ActivityTimeslot() {
     }
