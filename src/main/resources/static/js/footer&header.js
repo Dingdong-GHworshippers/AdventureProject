effa6e8cd712ce3d8ece26f88f7645a2e6432dce
Mr.Morsing14 <frmo0006@stud.ek.dk>
2025-10-06 11:11:55 +0200

    added header-footer styling and some empty html files

--- src/main/resources/static/js/footer&header.js
+++ src/main/resources/static/js/footer&header.js
@@ -3,9 +3,11 @@ class AppHeader extends HTMLElement {
         this.innerHTML = `
       <header>
         <nav>
-          <a href="booking.html">Home</a>
-          <a href="about.html">About</a>
-          <a href="contact.html">Contact</a>
+          <a href="index.html">Forside</a>
+          <a href="booking.html">Booking</a>
+          <a href="contact.html">Kontakt</a>
+          <a href="gallery.html">Galleri</a>
+
         </nav>
       </header>
     `;
@@ -17,7 +19,104 @@ class AppFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
       <footer>
-        <p>&copy; 2025 Wishify ApS — All rights reserved</p>
+        <div class="footerMainDiv">
+  <div class="column">
+    <a href="AboutUs.html"> Kort om os </a>
+    <p>
+      Hos Adventure Alley brænder vi for at skabe uforglemmelige oplevelser for både private og virksomheder.
+    Vi tilbyder actionfyldte aktiviteter som gokart, paintball, sumobrydning og minigolf – alt sammen designet til at styrke fællesskab, konkurrence og grin.
+    Vores mål er at give dig en dag fyldt med energi, sammenhold og sjove udfordringer, uanset om du kommer alene, med familien eller som del af et team.
+    </p>
+  </div>
+
+  <div id="center" class="column">
+    <a href="gallery.html"> Se nogle billeder fra glade familier her! </a>
+    <img id="padding-for-volunteer-image" src="/static/img/gocart.jpg">
+  </div>
+
+  <div class="socials-column">
+    <div class="socials-column-text-box">
+      <h1> Følg os på sociale medier </h1>
+    </div>
+
+    <div class="socials-column-row">
+      <a href="https://www.facebook.com/" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/fb.png">
+        </div>
+      </a>
+
+      <a href="https://www.instagram.com/" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/insta.png">
+        </div>
+      </a>
+
+
+      <a href="https://www.tiktok.com/" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/tiktok.png">
+        </div>
+      </a>
+
+    </div>
+
+    <div class="socials-column-row">
+      <a href="https://youtube.com/" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/youtube.png">
+        </div>
+      </a>
+
+
+      <a href="https://x.com/" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/x.png">
+        </div>
+      </a>
+
+
+      <a href="https://linkedin.com/" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/linkedin.png">
+        </div>
+      </a>
+    </div>
+
+    <div class="socials-column-row">
+      <a href="https://github.com/itsHarning/Projekt3_Gruppe_3" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/github.png">
+        </div>
+      </a>
+
+
+      <a href="https://bsky.app/" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/bluesky.png">
+        </div>
+      </a>
+
+
+      <a href="https://whatsapp.com/" target="_blank">
+        <div class="image-container-socials">
+          <img src="/static/img/whatsapp.png">
+        </div>
+      </a>
+    </div>
+
+  </div>
+
+  <div class="last-column">
+    <h2> Kontakt os </h2>
+    <h1> Adventure XP </h1>
+    <h1> Tlf: 69 69 69 69 </h1>
+    <h1> E-mail: info@adventurexp.dk </h1>
+    <p> Guldbergsgade 29N </p>
+    <p> 2200 København N </p>
+    <p> CVR: 420 1337 </p>
+  </div>
+</div>
       </footer>
     `;
     }
