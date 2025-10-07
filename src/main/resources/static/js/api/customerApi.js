422cc177f421816e225c9b396ce9dfe792d3fae6
juliusbay <juax0001@stud.kea.dk>
2025-10-07 17:42:46 +0200

    added booking api and customer api js methods

---
+++ src/main/resources/static/js/api/customerApi.js
@@ -0,0 +1,22 @@
+import {get, del, put, post} from "../util/fetch.js";
+
+const BASE_URL = "http://localhost:8080/api";
+const CUSTOMER_URL = `${BASE_URL}/customers`;
+
+export async function getCustomers(){
+    return get(CUSTOMER_URL);
+}
+
+export async function createCustomer(customer){
+    return post(CUSTOMER_URL, customer);
+}
+
+export async function updateCustomer(id, newCustomer){
+    return put(`${CUSTOMER_URL}/${id}`, newCustomer);
+}
+
+export async function deleteCustomer(id){
+    console.log(id  + id);
+
+    return del(`${CUSTOMER_URL}/${id}`);
+}
