7fab40ca8853a35d44f75783ef30476700623779
MrMorsing14 <frmo0006@stud.kea.dk>
2025-10-08 18:55:32 +0200

    employee api

---
+++ src/main/resources/static/js/api/employeeApi.js
@@ -0,0 +1,22 @@
+import {get, del, put, post} from "../util/fetch.js";
+
+const BASE_URL = "http://localhost:8080/api";
+const EMPLOYEE_URL = `${BASE_URL}/employees`;
+
+export async function getEmployees(){
+    return get(EMPLOYEE_URL);
+}
+
+export async function createEmployee(employee){
+    return post(EMPLOYEE_URL, employee);
+}
+
+export async function updateEmployee(id, newEmployee){
+    return put(`${EMPLOYEE_URL}/${id}`, newEmployee);
+}
+
+export async function deleteEmployee(id){
+    console.log(id  + id);
+
+    return del(`${EMPLOYEE_URL}/${id}`);
+}
